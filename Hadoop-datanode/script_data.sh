67f3b103d977d80ddeaadcdefce768946cfc8ecf
m1c4lv <miguelcarrilloalv@gmail.com>
2023-04-09 10:24:19 +0200

    Hadoop funcionando

--- Hadoop-datanode/script_data.sh
+++ Hadoop-datanode/script_data.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 $HADOOP_HOME/bin/hdfs datanode
-
--- Hadoop-namenode/script_name.sh
+++ Hadoop-namenode/script_name.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+##$HADOOP_HOME/bin/hdfs namenode -format
 $HADOOP_HOME/bin/hdfs namenode 
-##-format
-##$HADOOP_HOME/sbin/start-all.sh
+
