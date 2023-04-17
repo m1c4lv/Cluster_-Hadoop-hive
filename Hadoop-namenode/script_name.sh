5120327e0be522636d8f5a1d6a8ee46c556c1828
m1c4lv <128991223+m1c4lv@users.noreply.github.com>
2023-04-17 17:40:16 +0200

    Update script_name.sh

--- Hadoop-namenode/script_name.sh
+++ Hadoop-namenode/script_name.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-##$HADOOP_HOME/bin/hdfs namenode -format
+if ! [ -d /opt/volume/namenode/current ]; then
+  echo "Formateando namenode:"
+  cd /opt/hadoop-3.3.5/bin/
+  hdfs namenode -format
+fi
 $HADOOP_HOME/bin/hdfs namenode 
 
